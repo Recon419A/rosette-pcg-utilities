281a7e8495a6b097f89219102104d19da1e6f8f4
Drew Bronson <bronsonboy96@gmail.com>
2017-06-08 19:29:28 -0500

    Cleanup - remove reference to deleted function.

--- choice.rkt
+++ choice.rkt
@@ -3,7 +3,10 @@
 (require rosette/lib/angelic
          "assert.rkt")
 
-(provide choose-random random-instance)
+(provide choose-random)
+
+;; choose* breaks with less than three items in the list
+;; this appears to be a bug in rosette, so this is a workaround
 
 (define (choose-random choices)
   (let ([choice (apply choose* (shuffle (cons (gensym) (cons (gensym) choices))))])
