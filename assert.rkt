d95e3211ea1432c4dcd679b478e8f20967dca930
Drew Bronson <bronsonboy96@gmail.com>
2017-06-08 19:34:55 -0500

    Reformat to 80-character width.

--- assert.rkt
+++ assert.rkt
@@ -17,9 +17,11 @@
 (define-syntax (in! stx)
   (match (syntax->list stx)
     [(list name symbol possible-values)
-     (datum->syntax stx `(assert (member ,symbol (shuffle ,possible-values ))))]))
+     (datum->syntax
+      stx `(assert (member ,symbol (shuffle ,possible-values ))))]))
 
 (define-syntax (not-in! stx)
   (match (syntax->list stx)
     [(list name symbol impossible-values)
-     (datum->syntax stx `(assert (not (member ,symbol (shuffle ,impossible-values )))))]))
+     (datum->syntax
+      stx `(assert (not (member ,symbol (shuffle ,impossible-values )))))]))
