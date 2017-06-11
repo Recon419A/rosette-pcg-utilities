333624036522ea2e7470eeaa3b7cb0cec85123e9
Drew Bronson <bronsonboy96@gmail.com>
2017-06-11 14:24:46 -0500

    Implement tests for choose-random.

--- test/test-choice.rkt
+++ test/test-choice.rkt
@@ -1,3 +1,9 @@
 #lang rosette
 
-(require "../source/choice.rkt")
+(require rackunit
+         "../source/choice.rkt")
+
+(check-equal? (evaluate (choose-random '(0)) (solve asserts)) 0)
+(check member (evaluate (choose-random '(0 1)) (solve asserts)) '(0 1))
+(check member (evaluate (choose-random '(0 1 2 3)) (solve asserts)) '(0 1 2 3))
+
